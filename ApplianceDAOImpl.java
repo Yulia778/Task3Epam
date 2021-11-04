52e2af5ae00b72d0c8dc684502bb33a02e2f8b81
Yulia778 <84283264+Yulia778@users.noreply.github.com>
2021-11-04 11:57:09 +0300

    Add files via upload

---
+++ ApplianceDAOImpl.java
@@ -0,0 +1,27 @@
+package by.minsk.epam.kozlova.task2.java.dao.impl;
+
+public class ApplianceDAOImpl {
+    import java.util.ArrayList;
+import java.util.List;
+
+    public class ApplianceDAOImpl implements ApplianceDAO {
+        private static final String DB_FILE_NAME = "bd.xml";
+        ApplianceCatalogParser applianceCatalogParser = new ApplianceCatalogXMLParserImpl();
+        ApplianceMatcherProvider applianceMatcherProvider = ApplianceMatcherProvider.getInstance();
+
+        @Override
+        public List<Appliance> find(Criteria criteria) {
+            List<Appliance> appliances;
+            List<Appliance> result = new ArrayList<>();
+            appliances = applianceCatalogParser.readCatalogFromFile(DB_FILE_NAME);
+            for (Appliance appliance : appliances) {
+                Boolean isPassed = false;
+                ApplianceMatcher<? extends Appliance> applianceMatcher = applianceMatcherProvider.getApplianceMatcher(appliance.getClass());
+                if (applianceMatcher.matchAppliance(appliance, criteria)) {
+                    result.add(appliance);
+                }
+            }
+            return result;
+        }
+    }
+}
